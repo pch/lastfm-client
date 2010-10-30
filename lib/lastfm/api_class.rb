1998d8133b109781ad006331d4dc9ad7dfba7258
Piotr Chmolowski <piotr@chmolowski.pl>
2010-10-31 00:33:29 +0200

    method_missing for class methods

--- lib/lastfm/api_class.rb
+++ lib/lastfm/api_class.rb
@@ -1,13 +1,13 @@
 module LastFM
   class APIClass
-    def method_missing(method, params = {})
-      raise RuntimeError,  "Undefined method #{method}" unless self.class.allowed_methods.include?(method)
+    def self.method_missing(method, params = {})
+      raise RuntimeError,  "Undefined method #{method}" unless allowed_methods.include?(method)
       raise ArgumentError, "Params should be a hash"    unless params.is_a?(Hash)
 
-      api_method  = self.class.to_s.split("::").last + "." # LastFM::Album => Album
+      api_method  = self.to_s.split("::").last + "." # LastFM::Album => Album
       api_method += method.to_s.tr('_', '')                # get_info => getinfo
 
-      LastFM.send_api_request(api_method, params)
+      LastFM.send_api_request(api_method.downcase, params)
     end
 
     def self.allowed_methods(*args)
