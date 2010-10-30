1343c3f72672a0404de02c0608fb1ee3cfc5ed3e
Piotr Chmolowski <piotr@chmolowski.pl>
2010-10-31 00:34:13 +0200

    Allowed methods are class methods now

--- spec/api_class_spec.rb
+++ spec/api_class_spec.rb
@@ -7,19 +7,15 @@ describe LastFM::APIClass do
   end
 
   it "should not allow to call an undefined method" do
-    cl = LastFM::APIClass.new
-    lambda { cl.i_wasnt_defined }.should raise_error(RuntimeError, "Undefined method i_wasnt_defined")
+    lambda { LastFM::APIClass.i_wasnt_defined }.should raise_error(RuntimeError, "Undefined method i_wasnt_defined")
   end
 
   it "should not allow to call a method with invalid params" do
-    cl = LastFM::APIClass.new
-    lambda { cl.foo("not a hash") }.should raise_error(ArgumentError, "Params should be a hash")
+    lambda { LastFM::APIClass.foo("not a hash") }.should raise_error(ArgumentError, "Params should be a hash")
   end
 
   it "should respond to allowed methods" do
-    LastFM.should_receive(:send_api_request).with("APIClass.foo", {:bar=>:baz}).and_return({})
-
-    cl = LastFM::APIClass.new
-    cl.foo(:bar => :baz).should be_a(Hash)
+    LastFM.should_receive(:send_api_request).with("apiclass.foo", {:bar=>:baz}).and_return({})
+    LastFM::APIClass.foo(:bar => :baz).should be_a(Hash)
   end
 end
