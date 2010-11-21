7fce257c10498c9eb773b0226afe9b088c7b5ac5
Piotr Chmolowski <piotr@chmolowski.pl>
2010-11-21 10:14:44 +0100

    Check restricted method definitions in Artist

--- spec/api_classes/artist_spec.rb
+++ spec/api_classes/artist_spec.rb
@@ -2,10 +2,14 @@ require "spec_helper"
 
 describe LastFM::Artist do
   it "should define unrestricted methods" do
-    LastFM::Artist.unrestricted_methods.should == [:get_correction, :get_events, :get_images, :get_info, :get_past_events, :get_podcast, :get_shouts, :get_similar, :get_top_albums, :get_top_fans, :get_top_tags, :gettoptracks, :search]
+    LastFM::Artist.should respond_to(
+      :get_correction, :get_events, :get_images, :get_info, :get_past_events,
+      :get_podcast, :get_shouts, :get_similar, :get_top_albums, :get_top_fans,
+      :get_top_tags, :gettoptracks, :search
+    )
   end
-
+  
   it "should define restricted methods" do
-    LastFM::Artist.restricted_methods.should == [:add_tags, :get_tags, :remove_tag, :share, :shout]
+    LastFM::Artist.should respond_to(:add_tags, :get_tags, :remove_tag, :share, :shout)
   end
 end
