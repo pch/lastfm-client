ab380f5262a6f0c1ebbd694d225f6d8a1fe0823c
Piotr Chmolowski <piotr@chmolowski.pl>
2010-11-21 10:28:22 +0100

    Define write methods

--- lib/lastfm/api_classes/library.rb
+++ lib/lastfm/api_classes/library.rb
@@ -1,6 +1,9 @@
 module LastFM
   class Library < APIClass
     unrestricted_methods :get_albums, :get_artists, :get_tracks
-    restricted_methods   :add_album, :add_artist, :add_track
+    
+    restricted_methods do 
+      write :add_album, :add_artist, :add_track
+    end
   end
 end
