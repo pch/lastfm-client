0302ef4fb6af8ea8d9d8e5c4f69d11ff5aacd427
Piotr Chmolowski <piotr@chmolowski.pl>
2010-11-21 10:29:28 +0100

    Check restricted method definitions for Library

--- spec/api_classes/library_spec.rb
+++ spec/api_classes/library_spec.rb
@@ -2,10 +2,10 @@ require "spec_helper"
 
 describe LastFM::Library do
   it "should define unrestricted methods" do
-    LastFM::Library.unrestricted_methods.should == [:get_albums, :get_artists, :get_tracks]
+    LastFM::Library.should respond_to(:get_albums, :get_artists, :get_tracks)
   end
 
   it "should define restricted methods" do
-    LastFM::Library.restricted_methods.should == [:add_album, :add_artist, :add_track]
+    LastFM::Library.should respond_to(:add_album, :add_artist, :add_track)
   end
 end
