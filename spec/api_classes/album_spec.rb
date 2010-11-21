34b90e9bf51b47221bf0bdde701fc709451fdebc
Piotr Chmolowski <piotr@chmolowski.pl>
2010-11-21 10:09:55 +0100

    Add request_method to send_api_request

--- spec/api_classes/album_spec.rb
+++ spec/api_classes/album_spec.rb
@@ -6,18 +6,18 @@ describe LastFM::Album do
   end
 
   it "should respond to unrestricted methods" do
-    LastFM.should_receive(:send_api_request).with("album.getinfo", {:bar => :baz}).and_return({})
+    LastFM.should_receive(:send_api_request).with("album.getinfo", {:bar => :baz}, :get).and_return({})
     LastFM::Album.get_info(:bar => :baz).should be_a(Hash)
   end
 
   describe "restricted methods" do
     it "should respond to restricted read methods" do
-      LastFM.should_receive(:send_api_request).with("album.gettags", {:bar => :baz, :api_sig=>true}).and_return({})
+      LastFM.should_receive(:send_api_request).with("album.gettags", {:bar => :baz, :api_sig => true}, :get).and_return({})
       LastFM::Album.get_tags(:bar => :baz).should be_a(Hash)
     end
 
     it "should respond to restricted write methods" do
-      LastFM.should_receive(:send_api_request).with("album.addtags", {:bar => :baz, :api_sig=>true}).and_return({})
+      LastFM.should_receive(:send_api_request).with("album.addtags", {:bar => :baz, :api_sig => true}, :post).and_return({})
       LastFM::Album.add_tags(:bar => :baz).should be_a(Hash)
     end
   end
