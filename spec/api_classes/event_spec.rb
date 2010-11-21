f132e7d7ee170d4e21360abaf8a830ac1870a6a9
Piotr Chmolowski <piotr@chmolowski.pl>
2010-11-21 10:24:04 +0100

    Check restricted method definitions for Event

--- spec/api_classes/event_spec.rb
+++ spec/api_classes/event_spec.rb
@@ -2,10 +2,10 @@ require "spec_helper"
 
 describe LastFM::Event do
   it "should define unrestricted methods" do
-    LastFM::Event.unrestricted_methods.should == [:get_attendees, :get_info, :get_shouts]
+    LastFM::Event.should respond_to(:get_attendees, :get_info, :get_shouts)
   end
 
   it "should define restricted methods" do
-    LastFM::Event.restricted_methods.should == [:attend, :share, :shout]
+    LastFM::Event.should respond_to(:attend, :share, :shout)
   end
 end
