35d5f03d37cb1dab286040c5ab8e9353fd38d1bb
Piotr Chmolowski <piotr@chmolowski.pl>
2010-11-21 10:40:09 +0100

    Check method definitions for Tag

--- spec/api_classes/tag_spec.rb
+++ spec/api_classes/tag_spec.rb
@@ -2,6 +2,6 @@ require "spec_helper"
 
 describe LastFM::Tag do
   it "should define unrestricted methods" do
-    LastFM::Tag.unrestricted_methods.should == [:get_similar, :get_top_albums, :get_top_artists, :get_top_tags, :get_top_tracks, :get_weekly_artist_chart, :get_weekly_chart_list, :search]
+    LastFM::Tag.should respond_to(:get_similar, :get_top_albums, :get_top_artists, :get_top_tags, :get_top_tracks, :get_weekly_artist_chart, :get_weekly_chart_list, :search)
   end
 end
