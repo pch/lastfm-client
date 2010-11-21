8bb85b162891e52f263abb1202ad069df44257b1
Piotr Chmolowski <piotr@chmolowski.pl>
2010-11-21 10:38:17 +0100

    Check method definitions for Radio

--- spec/api_classes/radio_spec.rb
+++ spec/api_classes/radio_spec.rb
@@ -1,7 +1,11 @@
 require "spec_helper"
 
 describe LastFM::Radio do
+  it "should define unrestricted methods" do
+    LastFM::Radio.should respond_to(:search)
+  end
+  
   it "should define restricted methods" do
-    LastFM::Radio.restricted_methods.should == [:get_playlist, :tune]
+    LastFM::Radio.should respond_to(:get_playlist, :tune)
   end
 end
