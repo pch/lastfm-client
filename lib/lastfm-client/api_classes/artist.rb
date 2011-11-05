1940446ec0e04c7fbd7bee70992c9dceb7928172
magnolia-fan <magnolia_fan@me.com>
2011-11-05 16:05:52 +0400

    API fixes

--- lib/lastfm-client/api_classes/artist.rb
+++ lib/lastfm-client/api_classes/artist.rb
@@ -2,7 +2,7 @@ module LastFM
   class Artist < APIClass
     unrestricted_methods :get_correction, :get_events, :get_images, :get_info, :get_past_events,
                          :get_podcast, :get_shouts, :get_similar, :get_top_albums, :get_top_fans,
-                         :get_top_tags, :gettoptracks, :search
+                         :get_top_tags, :get_top_tracks, :search
 
     restricted_methods do
       read  :get_tags
